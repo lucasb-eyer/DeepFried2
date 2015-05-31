9fab2761be16287c5805fca797b007e54dccfaac
lucasb-eyer <lucasb.eyer.be@gmail.com>
2015-05-31 07:26:33 +0200

    Make Kaggle-Otto a bit cleaner.
    This should not change any behaviour.

--- examples/Kaggle-Otto/run.py
+++ examples/Kaggle-Otto/run.py
@@ -5,15 +5,14 @@ import beacon8.optimizers as optim
 from os.path import dirname, join as pjoin
 from sklearn.preprocessing import LabelEncoder
 from sklearn.cross_validation import train_test_split
-from train import *
-from test import *
+from train import train
+from test import validate
 
 
 def load_train_data():
     train_data = pd.read_csv(pjoin(dirname(__file__), 'data', 'train.csv'))
     labels = train_data.target.values
-    labels_encoder = LabelEncoder()
-    labels = labels_encoder.fit_transform(labels)
+    labels = LabelEncoder().fit_transform(labels)
     train_data = train_data.drop('id', axis=1)
     train_data = train_data.drop('target', axis=1)
     return train_data.as_matrix(), labels
@@ -56,9 +55,9 @@ if __name__ == "__main__":
 
     optimiser = optim.Momentum(lr=0.01, momentum=0.9)
 
-    for epoch in range(1000):
+    for epoch in range(1, 1001):
         model.training()
-        if epoch > 100 and epoch % 100 == 0:
+        if epoch % 100 == 0:
             optimiser.hyperparams['lr'] /= 10
         train(train_data_x, train_data_y, model, optimiser, criterion, epoch, 100)
         train(train_data_x, train_data_y, model, optimiser, criterion, epoch, 100, 'stat')
--- examples/Kaggle-Otto/test.py
+++ examples/Kaggle-Otto/test.py
@@ -11,7 +11,7 @@ def validate(dataset_x, dataset_y, model, epoch, batch_size):
 
     mini_batch_input = np.empty(shape=(batch_size, 93), dtype=_th.config.floatX)
     mini_batch_targets = np.empty(shape=(batch_size, ), dtype=_th.config.floatX)
-    accuracy = 0.
+    logloss = 0.
 
     for j in range((dataset_x.shape[0] + batch_size - 1) // batch_size):
         progress.update(j * batch_size)
@@ -26,7 +26,7 @@ def validate(dataset_x, dataset_y, model, epoch, batch_size):
             mini_batch_prediction.resize((dataset_x.shape[0] - j * batch_size, 9))
             mini_batch_targets.resize((dataset_x.shape[0] - j * batch_size, ))
 
-        accuracy = accuracy + multiclass_log_loss(mini_batch_targets, mini_batch_prediction, normalize=False)
+        logloss += multiclass_log_loss(mini_batch_targets, mini_batch_prediction, normalize=False)
 
     progress.finish()
-    print("Epoch #" + str(epoch) + ", Logloss: " + str(float(accuracy) / dataset_x.shape[0]))
+    print("Epoch #{}, Logloss: {:.5f}".format(epoch, logloss/dataset_x.shape[0]))
