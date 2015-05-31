5f4ee6cb9c14f481d5f1e1666e581f6baacd8cc9
lucasb-eyer <lucasb.eyer.be@gmail.com>
2015-05-31 03:17:52 +0200

    MNIST example works when called outside folder.

--- examples/MNIST/mnist.py
+++ examples/MNIST/mnist.py
@@ -13,7 +13,7 @@ except ImportError:
 '''Adapted from theano tutorial'''
 
 
-def load_mnist(data_file = './mnist.pkl.gz'):
+def load_mnist(data_file = os.path.join(os.path.dirname(__file__), 'mnist.pkl.gz')):
 
     if not os.path.exists(data_file):
         origin = ('http://www.iro.umontreal.ca/~lisa/deep/data/mnist/mnist.pkl.gz')
