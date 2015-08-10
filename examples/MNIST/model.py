712200235e770125c2b39210ed6de2129034e6fb
Alexander Hermans <hermans@vision.rwth-aachen.de>
2015-08-10 15:20:02 +0200

    Removed CUDnn requirements from the example model.
    The old model is now called lenet_cudnn, the new one is similar, but without the CUDnn convolutions.

--- examples/MNIST/model.py
+++ examples/MNIST/model.py
@@ -17,7 +17,7 @@ def net():
     return model
 
 
-def lenet():
+def lenet_cudnn():
     model = df.Sequential()
     model.add(df.Reshape(-1, 1, 28, 28))
     model.add(df.SpatialConvolutionCUDNN(1, 32, 5, 5, 1, 1, 2, 2, with_bias=False))
@@ -40,3 +40,26 @@ def lenet():
     model.add(df.SoftMax())
     return model
 
+
+def lenet():
+    model = df.Sequential()
+    model.add(df.Reshape(-1, 1, 28, 28))
+    model.add(df.SpatialConvolution(1, 32, 5, 5, 1, 1, with_bias=False))
+    model.add(df.BatchNormalization(32))
+    model.add(df.ReLU())
+    model.add(df.SpatialMaxPooling(2, 2))
+
+    model.add(df.SpatialConvolution(32, 64, 5, 5, 1, 1, with_bias=False))
+    model.add(df.BatchNormalization(64))
+    model.add(df.ReLU())
+    model.add(df.SpatialMaxPooling(2, 2))
+    model.add(df.Reshape(-1, 4*4*64))
+
+    model.add(df.Linear(4*4*64, 100, with_bias=False))
+    model.add(df.BatchNormalization(100))
+    model.add(df.ReLU())
+    model.add(df.Dropout(0.5))
+
+    model.add(df.Linear(100, 10))
+    model.add(df.SoftMax())
+    return model
