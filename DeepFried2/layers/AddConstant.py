0f82921e303b0aac87fae10e7ed75834ad4d6b6c
lucasb-eyer <lucasb.eyer.be@gmail.com>
2015-09-03 08:34:34 +0200

    Oops, fix `AddConstant` to call Module constructor.

--- DeepFried2/layers/AddConstant.py
+++ DeepFried2/layers/AddConstant.py
@@ -3,6 +3,7 @@ import DeepFried2 as df
 
 class AddConstant(df.Module):
     def __init__(self, scalar):
+        df.Module.__init__(self)
         self.scalar = scalar
 
     def symb_forward(self, symb_input):
