28fc199d5f372ea2bf2fcb472c5acfbcffe52e63
Isaak <isaak.lim@rwth-aachen.de>
2015-09-15 11:16:09 +0200

    added option to use leaky RELU

--- DeepFried2/layers/ReLU.py
+++ DeepFried2/layers/ReLU.py
@@ -1,7 +1,12 @@
 import DeepFried2 as df
 
+from theano.tensor.nnet import relu
 
 class ReLU(df.Module):
 
+    def __init__(self, alpha = 0):
+        df.Module.__init__(self)
+        self.alpha = alpha
+
     def symb_forward(self, symb_input):
-        return (symb_input + abs(symb_input)) * 0.5
+        return relu(symb_input, self.alpha)
