31a009d22f92cf3aa9fe7ebe1fc2d6dbb8445d95
Isaak <isaak.lim@rwth-aachen.de>
2015-10-02 11:58:23 +0200

    added 3D spatial softmax for SpatialSoftMaxCUDNN

--- DeepFried2/layers/SpatialSoftMaxCUDNN.py
+++ DeepFried2/layers/SpatialSoftMaxCUDNN.py
@@ -2,8 +2,24 @@ import DeepFried2 as df
 
 
 def spatial_softmax(img, algo, mode):
-    img = df.th.sandbox.cuda.basic_ops.gpu_contiguous(img)
-    return df.th.sandbox.cuda.dnn.GpuDnnSoftmax(tensor_format='bc01', algo=algo, mode=mode)(img)
+    # check if we have 3D input
+    if img.ndim == 5:
+        """symb_input shape: (n_input, depth, channels, height, width)"""
+        # shape: bdc01
+        vol_shape = img.shape
+        # shape: bcd01
+        vol = df.T.basic.swapaxes(img, 1, 2)
+        # shape: bcd(0*1)
+        vol = df.T.flatten(vol, outdim=4)
+        vol = df.th.sandbox.cuda.basic_ops.gpu_contiguous(vol)
+        res = df.th.sandbox.cuda.dnn.GpuDnnSoftmax(tensor_format='bc01', algo=algo, mode=mode)(vol)
+        # shape: bdc(0*1)
+        res = df.T.basic.swapaxes(res, 1, 2)
+        # shape: bdc01
+        return res.reshape(vol_shape)
+    else:
+        img = df.th.sandbox.cuda.basic_ops.gpu_contiguous(img)
+        return df.th.sandbox.cuda.dnn.GpuDnnSoftmax(tensor_format='bc01', algo=algo, mode=mode)(img)
 
 
 class SpatialSoftMaxCUDNN(df.Module):
