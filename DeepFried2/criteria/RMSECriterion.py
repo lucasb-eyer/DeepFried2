f3dc995a4ffefe18436ab3f69acb7c5603cf98b6
Isaak Lim <isaak.lim@rwth-aachen.de>
2015-12-22 13:58:15 +0100

    compute mean over the batch

--- DeepFried2/criteria/RMSECriterion.py
+++ DeepFried2/criteria/RMSECriterion.py
@@ -1,15 +1,16 @@
 import DeepFried2 as df
+import numpy as np
 
 
 class RMSECriterion(df.Criterion):
     def symb_forward(self, symb_input, symb_target):
         self._assert_same_dim(symb_input, symb_target)
 
-        return df.T.mean(df.T.sqrt(df.T.sum((symb_input - symb_target)**2, axis=1)))
+        return df.T.mean(df.T.sqrt(df.T.sum((symb_input - symb_target)**2, axis=np.arange(1,symb_input.ndim))))
 
 
 class MSECriterion(df.Criterion):
     def symb_forward(self, symb_input, symb_target):
         self._assert_same_dim(symb_input, symb_target)
 
-        return df.T.mean(df.T.sum((symb_input - symb_target)**2, axis=1))
+        return df.T.mean(df.T.sum((symb_input - symb_target)**2, axis=np.arange(1,symb_input.ndim)))
